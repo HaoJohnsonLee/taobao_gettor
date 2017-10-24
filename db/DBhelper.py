8bb3732b7a08cb98eb1dceaf1164792e29292733
记忆流年 <1486609892@qq.com>
2017-10-24 22:44:25 +0800

    fuzi

--- db/DBhelper.py
+++ db/DBhelper.py
@@ -4,7 +4,6 @@ import pymysql
 
 #connect=pymysql.Connect("*","*","*","*",charset='utf8')
 
-
 def insert_info(info_price,info_name,info_paynum):
     cursor=connect.cursor()
     sql="insert into nor_info(info_price, info_name, info_paynum) VALUES ('%s','%s','%d')" % \
